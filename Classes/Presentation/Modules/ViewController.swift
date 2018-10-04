64eaca50746780623f996bcc5713d5ea156ab56c
Nikita Zatsepilov <nikita.zatsepilov@rosberry.com>
2018-10-04 12:38:53 +0600

    Add bad code

--- Classes/Presentation/Modules/ViewController.swift
+++ Classes/Presentation/Modules/ViewController.swift
@@ -6,7 +6,25 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    enum BadEnum {
+        case first, second, third
+        case a, b, c
+
+    }
+
+    private enum AnyError: Error {
+        case any
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    private func reallyBadCode() {
+        let closure = {
+            throw AnyError.any
+        }
+        try! closure()
+
     }
 }
